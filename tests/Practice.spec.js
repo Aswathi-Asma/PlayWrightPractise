c0a30c377ca150037241fa7e8398bca1e1d97c15
Aswathi <alagarswamyaswathi99@gmail.com>
2026-08-23 18:31:55 +0530

    Few Changed

--- tests/Practice.spec.js
+++ tests/Practice.spec.js
@@ -1,10 +1,12 @@
-import{test , expect} from '@playwright/test';
-test('Practice' , async({page}) => {
+import { test, expect } from '@playwright/test';
+test('Practice', async ({ page }) => {
     await page.goto('https://practicetestautomation.com/practice-test-login/');
     await page.getByRole('textbox', { name: 'Username' }).fill('student');
-    await page.getByRole('textbox', {name: 'Password'}).fill('Password123');
-    await page.getByRole('button', {name: 'Submit'}).click();
-    await expect(page).toHaveTitle(/Logged In Successfully/);
-    await expect(page).toHaveTitle(/Logged In/);
+    await page.getByRole('textbox', { name: 'Password' }).fill('Password123');
+    await page.getByRole('button', { name: 'Submit' }).click();
+
+    await page.getByRole('link', { name: 'Home' }).click();
+    await page.getByRole('heading', { name: 'Hello' })
+
     await page.pause();
 })
